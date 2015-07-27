10fc284a6cb18d7229bbefe158b12194d297987e
Massimo Penzo <penzo.massimo@gmail.com>
2015-07-27 10:44:34 -0500

    first commit - massimo

---
+++ app.js
@@ -0,0 +1,35 @@
+/**
+ * Created by massimo on 7/27/15.
+ */
+
+
+/*defining namespace for the app*/
+var myapp = {
+    my_constructors: {
+        models: {},
+        collections: {},
+        views: {},
+        router: {}
+    },
+    my_objects: {
+        models: {},
+        collections: {},
+        views: {},
+        router: {}
+    }
+};
+
+
+console.log('backbone');
+
+
+/*models constructors*/
+
+
+/*collections constructors*/
+
+
+/*views constructors*/
+
+
+/*router*/
